8dfd1f545df213099cbc8a6a382913971c47acbc
Peter Diaz <dpeter999@gmail.com>
2026-06-23 04:03:39 -0300

    Actualizando archivos

---
+++ automation/saucedemo-cypress/cypress/support/commands.js
@@ -0,0 +1,8 @@
+// cypress/support/commands.js
+//
+// Acá se definen comandos custom de Cypress, por ejemplo:
+//
+// Cypress.Commands.add('loginViaApi', (username, password) => { ... });
+//
+// No tenemos comandos custom por ahora; los métodos de alto nivel
+// viven en los Page Objects (cypress/pages/).
---
+++ automation/saucedemo-cypress/cypress/support/e2e.js
@@ -0,0 +1,12 @@
+// cypress/support/e2e.js
+//
+// Este archivo se carga automáticamente antes de cada spec.
+// Es el lugar para:
+//   - Importar comandos custom (cypress/support/commands.js)
+//   - Configurar comportamiento global (ej. ignorar excepciones no controladas)
+//
+// Por ahora no necesitamos comandos custom, pero dejamos el archivo
+// presente porque Cypress lo requiere por defecto y es buena práctica
+// tenerlo listo para crecer el proyecto.
+
+import './commands';
